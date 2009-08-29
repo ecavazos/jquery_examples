dd130cb43766fc1653497711c44da0195fa91d3e
ecavazos <ejcavazos@gmail.com>
2009-08-28 23:21:17 -0700

    adding a new sample and modifying first sample

---
+++ simple_ajax_on_demand.js
@@ -0,0 +1,48 @@
+$(function() {
+  title('Simple Ajax On Demand');
+  onDemandLink();
+});
+
+function title (str) {
+  $('body').append('<h1>'+str+'</h1>');
+}
+
+function onDemandLink () {
+  $('body').append('<a href="" >Click me for some on-demand a-sync high jinks')
+    .andSelf().bind('click', onDemandLinkClickHandler);
+}
+
+function onDemandLinkClickHandler(event) {
+  getSampleJson();
+  event.preventDefault();
+  event.stopPropagation();
+}
+
+function getSampleJson() {
+  $.ajax({
+    url: 'sample.json',               // local json file with sample data
+    timeout: 2000,                    // 2 seconds
+    dataType: 'json',
+    type: 'GET',                      // default
+    success: function(data) {         // success callback
+      success(data);
+    },
+    error: function(data) {           // error callback
+      error(data);
+    }
+  });
+}
+
+function success (data) {
+  $('h1').after('<ul id="ninjas"></ul>');
+  
+  var ninjas = data.narutoverse;
+  
+  for(var i in ninjas) {
+    $('#ninjas').append('<li>'+ninjas[i].name+'</li');
+  }
+}
+
+function error (data) {
+  alert('error handling code goes here.');
+}
