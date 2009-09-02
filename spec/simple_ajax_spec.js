5ba75ec32458dce4855c796df631466406e78485
ecavazos <ejcavazos@gmail.com>
2009-09-01 22:16:40 -0700

    more specs

--- spec/simple_ajax_spec.js
+++ spec/simple_ajax_spec.js
@@ -21,7 +21,7 @@ Screw.Unit(function() {
         init('#dom_test');
       });
       
-      it('should add header to page with "Simple Ajax Example" as the value', function() {        
+      it('should add header to page with "Simple Ajax Example" as the value', function() {
         expect($('h1:first').text()).to(equal, 'Simple Ajax Example');
       });
     
@@ -30,6 +30,51 @@ Screw.Unit(function() {
       });
     });
     
+    describe('getSampleJson()', function() {
+      var ajaxMock;
+      
+      before(function() {
+        $.ajax = function(opts) {
+          ajaxMock = {
+            url: opts.url
+          };
+        };
+      });
+      
+      it('should set url to "sample.json"', function() {
+        getSampleJson();
+        expect(ajaxMock.url).to(equal, 'sample.json');
+      });
+      
+    });
+    
+    describe('success(data)', function() {
+      var data;
+      
+      before(function() {
+        data = {
+          narutoverse: [
+            {name: 'naruto'},{name: 'kakashi'}
+          ]
+        };
+      });
+      
+      it('should create an unordered list', function() {
+        success(data);
+        expect($('#ninjas')).to(match_selector, '#ninjas');
+      });      
+      
+      it('should output naruto first', function() {
+        success(data);
+        expect($('#ninjas li:first').text()).to(equal, 'naruto');
+      });
+      
+      it('should output kakashi last', function() {
+        success(data);
+        expect($('#ninjas li:last').text()).to(equal, 'kakashi');
+      });
+      
+    });
   });
   
 });
